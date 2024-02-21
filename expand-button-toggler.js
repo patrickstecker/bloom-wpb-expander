9cabef3903f93204e2c83dc187655cd527906b42
patrickstecker <stecker2000@gmail.com>
2024-02-21 09:55:31 -0600

    Update Usage to only use Classes for buttons

--- expand-button-toggler.js
+++ expand-button-toggler.js
@@ -1,19 +1,23 @@
 jQuery(document).ready(function($) {
     $('.expand-button').click(function() {
-        var expandId = $(this).attr('id');
-        var containerId = expandId.replace('button', 'container');
-        var closeId = expandId.replace('expand', 'close');
-        $('#' + containerId).toggle();
-        var closeButton = $('#' + closeId);
-        if (closeButton.length) {
-            closeButton.toggle();
+        var expandClasses = $(this).attr('class').split(' ');
+        var expandClass;
+        for (var i = 0; i < expandClasses.length; i++) {
+            if (expandClasses[i].startsWith('expand-button-id-')) {
+                expandClass = expandClasses[i].replace('-id', '');
+                break;
+            }
         }
-    });
-
-    $('.close-button').click(function() {
-        var closeId = $(this).attr('id');
-        var containerId = closeId.replace('close', 'expand').replace('button', 'container');
-        $('#' + containerId).hide();
-        $(this).hide();
+        var containerId = expandClass.replace('button', 'container');
+        var closeClass = expandClass.replace('expand', 'close');
+        $('#' + containerId).toggle();
+        var closeButton = $('.' + closeClass)
+        if (closeButton.length) {
+            closeButton.eq(0).toggle();
+            closeButton.click(function() {
+                $('#' + containerId).hide();
+                $(this).hide();
+            });
+        }
     });
 });
